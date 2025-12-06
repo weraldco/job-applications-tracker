5533092d475023f918fd3034196b45577958714e
weraldco <werald.opolento@gmail.com>
2025-12-06 11:57:52 +0800

    CHANGING PORT TO 8080

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,7 @@ import testRouter from './routes/test';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 // Allow frontend
 app.use(
