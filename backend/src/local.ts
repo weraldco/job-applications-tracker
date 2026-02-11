fb314b6be22b75298191b5f2fcb04aaed1324f55
weraldco <werald.opolento@gmail.com>
2026-02-11 20:03:11 +0800

    revision to local.ts

--- backend/src/local.ts
+++ backend/src/local.ts
@@ -1,7 +1,12 @@
 //local.ts
 import app from './app';
 const PORT = process.env.PORT || 8080;
+const RAILWAY_URL = process.env.RAILWAY_STATIC_URL; // automatically set by Railway
 
 app.listen(PORT, () => {
-	console.log(`Server is running in http://localhost:${PORT}`);
+	if (RAILWAY_URL) {
+		console.log(`API running at ${RAILWAY_URL}`);
+	} else {
+		console.log(`Server running on http://localhost:${PORT}`);
+	}
 });
